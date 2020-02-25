827eedbe4ae55cd3992536d782875d0581d721f0
Martin Wenske <wolllis@gmx.de>
2020-02-25 12:36:48 +0100

    Adding new things: BGW14, FGW14, FTD14, FTS14KS

--- bundles/org.openhab.binding.eltako/src/main/java/org/openhab/binding/eltako/internal/EltakoBindingConstants.java
+++ bundles/org.openhab.binding.eltako/src/main/java/org/openhab/binding/eltako/internal/EltakoBindingConstants.java
@@ -38,6 +38,7 @@ public class EltakoBindingConstants {
     public static final ThingTypeUID THING_TYPE_DSZ14DRS = new ThingTypeUID(BINDING_ID, "DSZ14DRS");
     public static final ThingTypeUID THING_TYPE_DSZ14WDRS = new ThingTypeUID(BINDING_ID, "DSZ14WDRS");
     public static final ThingTypeUID THING_TYPE_F2L14 = new ThingTypeUID(BINDING_ID, "F2L14");
+    public static final ThingTypeUID THING_TYPE_BGW14 = new ThingTypeUID(BINDING_ID, "BGW14");
     public static final ThingTypeUID THING_TYPE_F3Z14D = new ThingTypeUID(BINDING_ID, "F3Z14D");
     public static final ThingTypeUID THING_TYPE_F4HK14 = new ThingTypeUID(BINDING_ID, "F4HK14");
     public static final ThingTypeUID THING_TYPE_FAE14LPR = new ThingTypeUID(BINDING_ID, "FAE14LPR");
@@ -46,6 +47,7 @@ public class EltakoBindingConstants {
     public static final ThingTypeUID THING_TYPE_FDG14 = new ThingTypeUID(BINDING_ID, "FDG14");
     public static final ThingTypeUID THING_TYPE_FFR14 = new ThingTypeUID(BINDING_ID, "FFR14");
     public static final ThingTypeUID THING_TYPE_FGSM14 = new ThingTypeUID(BINDING_ID, "FGSM14");
+    public static final ThingTypeUID THING_TYPE_FGW14 = new ThingTypeUID(BINDING_ID, "FGW14");
     public static final ThingTypeUID THING_TYPE_FHK14 = new ThingTypeUID(BINDING_ID, "FHK14");
     public static final ThingTypeUID THING_TYPE_FMS14 = new ThingTypeUID(BINDING_ID, "FMS14");
     public static final ThingTypeUID THING_TYPE_FMSR14 = new ThingTypeUID(BINDING_ID, "FMSR14");
@@ -59,8 +61,10 @@ public class EltakoBindingConstants {
     public static final ThingTypeUID THING_TYPE_FSR14_4X = new ThingTypeUID(BINDING_ID, "FSR14-4x");
     public static final ThingTypeUID THING_TYPE_FSR14SSR = new ThingTypeUID(BINDING_ID, "FSR14SSR");
     public static final ThingTypeUID THING_TYPE_FSU14 = new ThingTypeUID(BINDING_ID, "FSU14");
+    public static final ThingTypeUID THING_TYPE_FTD14 = new ThingTypeUID(BINDING_ID, "FTD14");
     public static final ThingTypeUID THING_TYPE_FTN14 = new ThingTypeUID(BINDING_ID, "FTN14");
     public static final ThingTypeUID THING_TYPE_FTS14GBZ = new ThingTypeUID(BINDING_ID, "FTS14GBZ");
+    public static final ThingTypeUID THING_TYPE_FTS14KS = new ThingTypeUID(BINDING_ID, "FTS14KS");
     public static final ThingTypeUID THING_TYPE_FTS14TG = new ThingTypeUID(BINDING_ID, "FTS14TG");
     public static final ThingTypeUID THING_TYPE_FUD14_800W = new ThingTypeUID(BINDING_ID, "FUD14-800W");
     public static final ThingTypeUID THING_TYPE_FUD14 = new ThingTypeUID(BINDING_ID, "FUD14");
@@ -68,14 +72,15 @@ public class EltakoBindingConstants {
     public static final ThingTypeUID THING_TYPE_FWZ14_65A = new ThingTypeUID(BINDING_ID, "FWZ14-65A");
     public static final ThingTypeUID THING_TYPE_FZK14 = new ThingTypeUID(BINDING_ID, "FZK14");
 
-    public static final Set<ThingTypeUID> SUPPORTED_DEVICE_THING_TYPES_UIDS = new HashSet<>(Arrays.asList(
-            THING_TYPE_DSZ14DRS, THING_TYPE_DSZ14WDRS, THING_TYPE_F2L14, THING_TYPE_F3Z14D, THING_TYPE_F4HK14,
-            THING_TYPE_FAE14LPR, THING_TYPE_FAE14SSR, THING_TYPE_FDG14_BROADCAST, THING_TYPE_FDG14, THING_TYPE_FFR14,
-            THING_TYPE_FGSM14, THING_TYPE_FHK14, THING_TYPE_FMS14, THING_TYPE_FMSR14, THING_TYPE_FMZ14,
-            THING_TYPE_FPLG14, THING_TYPE_FPLT14, THING_TYPE_FSB14, THING_TYPE_FSDG14, THING_TYPE_FSG14_1_10V,
-            THING_TYPE_FSR14_2X, THING_TYPE_FSR14_4X, THING_TYPE_FSR14SSR, THING_TYPE_FSU14, THING_TYPE_FTN14,
-            THING_TYPE_FTS14GBZ, THING_TYPE_FTS14TG, THING_TYPE_FUD14_800W, THING_TYPE_FUD14, THING_TYPE_FWG14MS,
-            THING_TYPE_FWZ14_65A, THING_TYPE_FZK14));
+    public static final Set<ThingTypeUID> SUPPORTED_DEVICE_THING_TYPES_UIDS = new HashSet<>(
+            Arrays.asList(THING_TYPE_DSZ14DRS, THING_TYPE_DSZ14WDRS, THING_TYPE_F2L14, THING_TYPE_BGW14,
+                    THING_TYPE_F3Z14D, THING_TYPE_F4HK14, THING_TYPE_FAE14LPR, THING_TYPE_FAE14SSR,
+                    THING_TYPE_FDG14_BROADCAST, THING_TYPE_FDG14, THING_TYPE_FFR14, THING_TYPE_FGSM14, THING_TYPE_FGW14,
+                    THING_TYPE_FHK14, THING_TYPE_FMS14, THING_TYPE_FMSR14, THING_TYPE_FMZ14, THING_TYPE_FPLG14,
+                    THING_TYPE_FPLT14, THING_TYPE_FSB14, THING_TYPE_FSDG14, THING_TYPE_FSG14_1_10V, THING_TYPE_FSR14_2X,
+                    THING_TYPE_FSR14_4X, THING_TYPE_FSR14SSR, THING_TYPE_FSU14, THING_TYPE_FTD14, THING_TYPE_FTN14,
+                    THING_TYPE_FTS14GBZ, THING_TYPE_FTS14KS, THING_TYPE_FTS14TG, THING_TYPE_FUD14_800W,
+                    THING_TYPE_FUD14, THING_TYPE_FWG14MS, THING_TYPE_FWZ14_65A, THING_TYPE_FZK14));
 
     // List of all FUD14 channel IDs
     public static final String CHANNEL_BRIGHTNESS = "brightness";

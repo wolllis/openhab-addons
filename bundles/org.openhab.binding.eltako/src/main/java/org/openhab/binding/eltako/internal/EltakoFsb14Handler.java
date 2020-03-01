1043683f016393dfb013eb8882686a70399872ac
Martin Wenske <wolllis@gmx.de>
2020-03-01 12:04:27 +0100

    Update documentation of FSB14

--- bundles/org.openhab.binding.eltako/src/main/java/org/openhab/binding/eltako/internal/EltakoFsb14Handler.java
+++ bundles/org.openhab.binding.eltako/src/main/java/org/openhab/binding/eltako/internal/EltakoFsb14Handler.java
@@ -18,14 +18,32 @@ import org.eclipse.smarthome.core.types.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+ * Direktes Fahrkommando mit Angabe der Laufzeit in Sek. FUNC = 3F, Typ = 7F (universal). Für jeden Kanal separat.
+ *
+ * ORG        = 0x07
+ * Data_byte3 = Laufzeit in 100ms MSB
+ * Data_byte2 = Laufzeit in 100ms LSB, oder Laufzeit in Sekunden 1-255 dez., die Laufzeiteinstellung am Gerät wird ignoriert.
+ * Data_byte1 = Kommando:
+ *              0x00 = Stopp
+ *              0x01 = Auf
+ *              0x02 = Ab
+ * Data_byte0 = DB0_Bit3 = LRN Button
+ *              (0 = Lerntelegramm, 1 = Datentelegramm)
+ *              DB0_Bit2 = Aktor für Taster blockieren/freigeben
+ *              (0 = freigeben, 1 = blockieren)
+ *              DB0_Bit1 = Umschaltung Laufzeit in Sekunden oder in 100ms.
+ *              (0 = Laufzeit nur in DB2 in Sekunden)
+ *              (1 = Laufzeit in DB3(MSB)+DB2(LSB) in 100ms.)
+ *
+ * Lerntelegramm DB3..DB0 muss so aussehen: 0xFF, 0xF8, 0x0D, 0x80
+ *
+ * Mit eingelernten Tastern kann jederzeit unterbrochen werden!
+*/
 /**
  * The {@link EltakoFsb14Handler} is responsible for processing device specific commands.
  *
  * @author Martin Wenske - Initial contribution
- */
-/**
- * @author martin
- *
  */
 public class EltakoFsb14Handler extends EltakoGenericHandler {
 
